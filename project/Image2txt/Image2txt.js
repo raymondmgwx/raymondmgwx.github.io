e4bee410b601dfbe4d2ce95c4bd14d2709939667
オウ　キョク <m02171017e@edu.teu.ac.jp>
2017-08-17 16:19:57 +0900

    change download func

--- project/Image2txt/Image2txt.js
+++ project/Image2txt/Image2txt.js
@@ -16,7 +16,7 @@ img.onload = init; // init
 
 //bind event
 fileBtn.onchange = getImg;
-//downloadBtn.onclick = saveImg;
+downloadBtn.onclick = downloadFile;
 
 //generate txt according to gray value
 function toText(g) {
@@ -75,20 +75,37 @@ function init() {
     saveImg2MobileAndPC();
 }
 
+function base64Img2Blob(code){
+    var parts = code.split(';base64,');
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+    
+    var uInt8Array = new Uint8Array(rawLength);
+    
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+    
+    return new Blob([uInt8Array], {type: contentType});
+}
+
+function downloadFile(fileName, content){
+    
+    var aLink = document.createElement('a');
+    var blob = base64Img2Blob(content); //new Blob([content]);
+    
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("click", false, false);
+    aLink.download = fileName;
+    aLink.href = URL.createObjectURL(blob);
+    
+    aLink.dispatchEvent(evt);
+}
 
 function saveImg2MobileAndPC() {
     
-    var type = 'png';
-    var imgdata = txt_div.toDataURL(type);
-    
-    var fixtype = function (type) {
-        type = type.toLocaleLowerCase().replace(/jpg/i, 'jpeg');
-        var r = type.match(/png|jpeg|bmp|gif/)[0];
-        return 'image/' + r;
-    }
-    imgdata = imgdata.replace(fixtype(type), 'image/octet-stream')
-    
-    downloadImg.href=imgdata;
+    downloadFile('img2txt.png', txt_div.toDataURL("image/png"));
 }
 
 //save image
