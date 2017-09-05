b6fdd1451926476f1b6a31f82f104e3af7c320ff
RaymondMcGuire <raymondmg@foxmail.com>
2017-09-06 05:23:27 +0900

    load2

--- psftjs/psftjs.view._webgl.js
+++ psftjs/psftjs.view._webgl.js
@@ -32,7 +32,7 @@ WRE.view._webgl._view._init = function (callback) {
         } else {
             WRE.view._webgl._projectFile += getReqQuery().e+"/"+getReqQuery().e;
             WRE.view._webgl._projectName = getReqQuery().e;
-            WRE.view._webgl._projectRootPath = WRE.view._webgl._projectFile + '.js';
+            WRE.view._webgl._projectRootPath = WRE.view._webgl._projectFile;
         }
         
         $.when(
@@ -41,7 +41,7 @@ WRE.view._webgl._view._init = function (callback) {
             WRE.ui.elements["webgl"].dom.innerHTML = canvasHtml;
 
             //$("head").append('<script type="text/javascript" src="' + WRE.view._webgl._projectRootPath + '.js" charset="utf-8"></script>');
-            loadScript(loc + WRE.view._webgl._projectRootPath , function () {
+            loadScript(loc + WRE.view._webgl._projectRootPath + '.js' , function () {
                 console.log("loaded project " + WRE.view._webgl._projectName + "  current time:" + new Date().getTime());
             });
 
