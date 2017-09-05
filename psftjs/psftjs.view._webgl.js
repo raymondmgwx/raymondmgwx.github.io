16f7dfc25b2f33bbc487b50c909f71ba45759cc9
RaymondMcGuire <raymondmg@foxmail.com>
2017-09-06 05:21:40 +0900

    load

--- psftjs/psftjs.view._webgl.js
+++ psftjs/psftjs.view._webgl.js
@@ -32,7 +32,7 @@ WRE.view._webgl._view._init = function (callback) {
         } else {
             WRE.view._webgl._projectFile += getReqQuery().e+"/"+getReqQuery().e;
             WRE.view._webgl._projectName = getReqQuery().e;
-            WRE.view._webgl._projectRootPath = WRE.view._webgl._projectFile;
+            WRE.view._webgl._projectRootPath = WRE.view._webgl._projectFile + '.js';
         }
         
         $.when(
@@ -40,10 +40,36 @@ WRE.view._webgl._view._init = function (callback) {
         ).then(function (canvasHtml) {
             WRE.ui.elements["webgl"].dom.innerHTML = canvasHtml;
 
-            $("head").append('<script type="text/javascript" src="' + WRE.view._webgl._projectRootPath + '.js" charset="utf-8"></script>');
+            //$("head").append('<script type="text/javascript" src="' + WRE.view._webgl._projectRootPath + '.js" charset="utf-8"></script>');
+            loadScript(loc + WRE.view._webgl._projectRootPath , function () {
+                console.log("loaded project " + WRE.view._webgl._projectName + "  current time:" + new Date().getTime());
+            });
 
-            console.log("loaded project " + WRE.view._webgl._projectName + "  current time:" + new Date().getTime());
+           
             callback();
         });
+
+
+        function loadScript(url, callback) {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            if (script.readyState) { //IE
+                script.onreadystatechange = function () {
+                    if (script.readyState == "loaded" || script.readyState == "complete") {
+                        script.onreadystatechange = null;
+                        //console.log(url + " loaded.");
+                        callback();
+                    }
+                };
+            } else { //Others
+                script.onload = function () {
+                    //console.log(url + " loaded.");
+                    callback();
+                };
+            }
+            //script.src = url;
+            script.src = url+ '?' + new Date().getTime();
+            document.getElementsByTagName("head")[0].appendChild(script);
+        }
     }
 };
