a49a8601e625189917939f74013cc08916d0a6dd
オウ　キョク <m02171017e@edu.teu.ac.jp>
2017-09-18 19:30:32 +0900

    style add

--- project/Blog-AnimeFacialPoint/Blog-AnimeFacialPoint.js
+++ project/Blog-AnimeFacialPoint/Blog-AnimeFacialPoint.js
@@ -19,7 +19,7 @@ function init() {
     img_div.width = img.width;
     img_div.height = img.height;
 
-    var left_offset = (img_window.width - img_div.width) / 2;
+    var left_offset = (img_window.style.width - img_div.width) / 2;
 
     img_canvas.drawImage(img, left_offset, 0);
 
