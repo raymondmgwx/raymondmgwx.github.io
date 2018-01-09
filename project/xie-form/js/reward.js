341fd7625246261b7dcece4bf68e90a4825e5bc9
オウ　キョク <m02171017e@edu.teu.ac.jp>
2018-01-09 17:34:13 +0900

    reward model delete and edit

--- project/xie-form/js/reward.js
+++ project/xie-form/js/reward.js
@@ -53,4 +53,98 @@ function load_reward_info() {
 $(function() {
 
     load_reward_info();
+    $('#delete-modal-reward').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget);
+        var id = button.data('id');
+        var name = button.data('name');
+        var sysid = button.data('sysid');
+        var modal = $(this);
+
+        modal.find('#user_id_reward').val(id);
+        modal.find('#reward_name').val(name);
+        modal.find('#reward_sysid').val(sysid);
+    });
+
+    $('#edit-modal-reward').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget);
+        var id = button.data('id');
+        var name = button.data('name');
+        var number = button.data('number');
+        var status = button.data('status');
+        var order_number = button.data('order_number');
+        var sysid = button.data('sysid');
+        var fee = button.data('fee');
+        var modal = $(this);
+
+        modal.find('#e_r_sysid').val(id);
+        modal.find('#e_r_name').val(name);
+        modal.find('#e_r_number').val(number);
+        modal.find('#e_r_status').val(status);
+        modal.find('#e_r_order').val(order_number);
+        modal.find('#e_r_fee').val(fee);
+        modal.find('#e_r_r_sysid').val(sysid);
+
+    });
+
+    $('#btn_delete_reward').click(function() {
+        $.get("https://api.myjson.com/bins/jpfbv", function(data, textStatus, jqXHR) {
+            var cur_json_info = data;
+            var reward_list = cur_json_info['reward_list'];
+            var del_index = 0;
+            var count = 0;
+            reward_list.forEach(element => {
+                if (element['reward_sysid'] == $('#reward_sysid').val()) {
+                    del_index = count;
+                }
+                count += 1;
+            });
+
+            reward_list.splice(del_index, 1);
+
+            var json_info = JSON.stringify(cur_json_info);
+            $.ajax({
+                url: "https://api.myjson.com/bins/jpfbv",
+                type: "PUT",
+                data: json_info,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function(data, textStatus, jqXHR) {
+                    alert('删除奖品信息成功');
+                    self.location = self.location;
+                }
+            });
+        });
+    });
+
+    $('#btn_edit_reward').click(function() {
+        $.get("https://api.myjson.com/bins/jpfbv", function(data, textStatus, jqXHR) {
+            var cur_json_info = data;
+            var reward_list = cur_json_info['reward_list'];
+
+            reward_list.forEach(element => {
+                if (element['reward_sysid'] == $('#e_r_r_sysid').val()) {
+                    element['reward_name'] = $('#e_r_name').val();
+                    element['reward_number'] = $('#e_r_number').val();
+                    element['status'] = $('#e_r_status').val();
+                    element['order_number'] = $('#e_r_order').val();
+                    element['fee'] = $('#e_r_fee').val();
+                }
+            });
+
+
+
+            var json_info = JSON.stringify(cur_json_info);
+            $.ajax({
+                url: "https://api.myjson.com/bins/jpfbv",
+                type: "PUT",
+                data: json_info,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function(data, textStatus, jqXHR) {
+                    alert('修改奖品成功!!!');
+                    self.location = self.location;
+                }
+            });
+        });
+    });
 });
