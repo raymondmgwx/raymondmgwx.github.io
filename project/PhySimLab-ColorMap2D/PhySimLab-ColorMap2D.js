4e5382358615e055c7278e42b54cca50004a4bfd
RaymondMcGuire <raymondmg@foxmail.com>
2018-03-19 01:01:43 +0900

    gaussian animation

--- project/PhySimLab-ColorMap2D/PhySimLab-ColorMap2D.js
+++ project/PhySimLab-ColorMap2D/PhySimLab-ColorMap2D.js
@@ -6,6 +6,17 @@
 //////////////////////////////////////  Last vist: 03, 16, 2018  by Raymond Wang                             //
 //////////////////////////////////////                                                                      //
 /////////////////////////////////////////////////////////////////////////////////////////////////WANG  XU///-->
+var rgb_scene = new THREE.Scene();
+var rgb_canvas = document.getElementById('canvas-rgb');
+var rgb_camera = new THREE.OrthographicCamera(-50, 50, 50, -50, 1, 100);
+var rgb_track = new THREE.TrackballControls(rgb_camera);
+var rgb_renderer = new THREE.WebGLRenderer({ antialias: true });
+
+var hsl_scene = new THREE.Scene();
+var hsl_canvas = document.getElementById('canvas-hsl');
+var hsl_camera = new THREE.OrthographicCamera(-50, 50, 50, -50, 1, 100);
+var hsl_track = new THREE.TrackballControls(hsl_camera);
+var hsl_renderer = new THREE.WebGLRenderer({ antialias: true });
 
 var gaussian_scene = new THREE.Scene();
 var gaussian_canvas = document.getElementById('canvas-gaussian');
@@ -13,16 +24,66 @@ var gaussian_camera = new THREE.OrthographicCamera(-50, 50, 50, -50, 1, 100);
 var gaussian_track = new THREE.TrackballControls(gaussian_camera);
 var gaussian_renderer = new THREE.WebGLRenderer({ antialias: true });
 
-
-var lg1 = new ColorMap_2D(gaussian_canvas, gaussian_track, gaussian_scene, gaussian_camera, gaussian_renderer);
+var rgbColorMap = new ColorMap_2D(rgb_canvas, rgb_track, rgb_scene, rgb_camera, rgb_renderer, "rgb");
+var hslColorMap = new ColorMap_2D(hsl_canvas, hsl_track, hsl_scene, hsl_camera, hsl_renderer, "hsl");
+var gaussianColorMap = new ColorMap_2D(gaussian_canvas, gaussian_track, gaussian_scene, gaussian_camera, gaussian_renderer, "gaussian");
+var gaussian_lattice = gaussianColorMap.lattice;
 
 
 function loop() {
 
-    gaussian_track.update();
+    rgb_track.update();
+    rgb_renderer.clear();
+    rgb_renderer.render(rgb_scene, rgb_camera);
+}
+
+function loop1() {
+    hsl_track.update();
+    hsl_renderer.clear();
+    hsl_renderer.render(hsl_scene, hsl_camera);
+}
+
+
+var step = 0;
+var omega = Math.PI / 50;
+var R = 20;
+var sigma = 300;
+var z_ = 50;
+var N = 100;
+var l = 1;
+
+function loop2() {
+    step++;
+    var colors = new Array();
+    var x_ = R * Math.cos(omega * step);
+    var y_ = R * Math.sin(omega * step);
+
+    for (var j = 0; j <= N; j++) {
+        for (var i = 0; i <= N; i++) {
+            var x = (-N / 2 + i) * l;
+            var y = (-N / 2 + j) * l;
+            var z = z_ * Math.exp(-((x - x_) * (x - x_) + (y - y_) * (y - y_)) / (2 * sigma));
+            colors.push(new THREE.Color().setRGB(z / z_, 0, 0));
+        }
+    }
+    var n = 0;
+    for (var j = 0; j < N; j++) {
+        for (var i = 0; i < N; i++) {
+            gaussian_lattice.geometry.faces[n].vertexColors[0].copy(colors[(N + 1) * j + i]);
+            gaussian_lattice.geometry.faces[n].vertexColors[1].copy(colors[(N + 1) * j + i + 1]);
+            gaussian_lattice.geometry.faces[n].vertexColors[2].copy(colors[(N + 1) * (j + 1) + i]);
+
+            gaussian_lattice.geometry.faces[n + 1].vertexColors[0].copy(colors[(N + 1) * (j + 1) + i]);
+            gaussian_lattice.geometry.faces[n + 1].vertexColors[1].copy(colors[(N + 1) * j + i + 1]);
+            gaussian_lattice.geometry.faces[n + 1].vertexColors[2].copy(colors[(N + 1) * (j + 1) + i + 1]);
+
+            n += 2;
+        }
+    }
+    gaussian_lattice.geometry.colorsNeedUpdate = true;
     gaussian_renderer.clear();
     gaussian_renderer.render(gaussian_scene, gaussian_camera);
-    requestAnimationFrame(loop);
+    //requestAnimationFrame(loop2);
 }
 
 function addScript() {
@@ -34,4 +95,6 @@ function addScript() {
 
 
 loop();
+loop1();
+loop2();
 addScript();
--- wre/lib/three-demo/wre.colormap2d.js
+++ wre/lib/three-demo/wre.colormap2d.js
@@ -10,13 +10,13 @@
 
 
 
-var ColorMap_2D = function(canvasElement, trackball, scene, camera, renderer) {
+var ColorMap_2D = function(canvasElement, trackball, scene, camera, renderer, color_type) {
     this.canvasElement = canvasElement;
     this.step = 0;
     this.initScene(renderer);
     this.initCamera(trackball, camera);
     this.initLight(scene);
-    this.initObject(scene);
+    this.initObject(scene, color_type);
 };
 
 ColorMap_2D.prototype = {
@@ -26,11 +26,10 @@ ColorMap_2D.prototype = {
         if (!renderer) alert('Three.js init failed!');
         renderer.setSize(canvasFrame.clientWidth, canvasFrame.clientHeight);
 
-        renderer.shadowMap.enabled = true;
-        renderer.shadowMap.type = THREE.PCFShadowMap;
+
         this.canvas = renderer.domElement;
         canvasFrame.appendChild(this.canvas);
-        renderer.setClearColor(new THREE.Color(0x000000, 1.0));
+
     },
     initCamera: function(trackball, camera) {
 
@@ -68,30 +67,114 @@ ColorMap_2D.prototype = {
         scene.add(ambientLight);
         scene.add(directionalLight);
     },
-    initObject: function(scene) {
+    initObject: function(scene, type) {
 
-        var geometry = new THREE.Geometry();
 
-        geometry.vertices[0] = new THREE.Vector3(-50, -50, 0);
-        geometry.vertices[1] = new THREE.Vector3(50, -50, 0);
-        geometry.vertices[2] = new THREE.Vector3(50, 50, 0);
-        geometry.vertices[3] = new THREE.Vector3(-50, 50, 0);
+        switch (type) {
+            case "rgb":
+                var geometry = new THREE.Geometry();
+                geometry.vertices[0] = new THREE.Vector3(-50, -50, 0);
+                geometry.vertices[1] = new THREE.Vector3(50, -50, 0);
+                geometry.vertices[2] = new THREE.Vector3(50, 50, 0);
+                geometry.vertices[3] = new THREE.Vector3(-50, 50, 0);
 
-        var colors1 = new Array();
-        colors1[0] = new THREE.Color(0xffffff);
-        colors1[1] = new THREE.Color(0xff0000);
-        colors1[2] = new THREE.Color(0x0000ff);
+                var colors1 = new Array();
+                colors1[0] = new THREE.Color(0xffffff);
+                colors1[1] = new THREE.Color(0xff0000);
+                colors1[2] = new THREE.Color(0x0000ff);
 
-        var colors2 = new Array();
-        colors2[0] = new THREE.Color(0x0000ff);
-        colors2[1] = new THREE.Color(0xff0000);
-        colors2[2] = new THREE.Color(0x00ff00);
+                var colors2 = new Array();
+                colors2[0] = new THREE.Color(0x0000ff);
+                colors2[1] = new THREE.Color(0xff0000);
+                colors2[2] = new THREE.Color(0x00ff00);
 
-        geometry.faces.push(new THREE.Face3(0, 1, 3, null, colors1));
-        geometry.faces.push(new THREE.Face3(3, 1, 2, null, colors2));
-        geometry.computeFaceNormals();
-        var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, vertexColors: THREE.VertexColors });
-        this.lattice = new THREE.Mesh(geometry, material);
-        scene.add(this.lattice);
+                geometry.faces.push(new THREE.Face3(0, 1, 3, null, colors1));
+                geometry.faces.push(new THREE.Face3(3, 1, 2, null, colors2));
+
+                var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, vertexColors: THREE.VertexColors });
+                this.lattice = new THREE.Mesh(geometry, material);
+                scene.add(this.lattice);
+                break;
+            case "hsl":
+                var geometry = new THREE.Geometry();
+                var colors = new Array();
+                var N = 100;
+                var l = 1;
+                for (var j = 0; j <= N; j++) {
+                    for (var i = 0; i <= N; i++) {
+                        var x = (-N / 2 + i) * l;
+                        var y = (-N / 2 + j) * l;
+                        var vertex = new THREE.Vector3(x, y, 0);
+                        geometry.vertices.push(vertex);
+                        var H = i / N;
+                        var S = 1;
+                        var L = j / N;
+                        colors.push(new THREE.Color().setHSL(H, S, L));
+                    }
+                }
+
+                for (var j = 0; j < N; j++) {
+                    for (var i = 0; i < N; i++) {
+                        var color1 = [];
+                        var color2 = [];
+                        color1[0] = colors[(N + 1) * j + i];
+                        color1[1] = colors[(N + 1) * j + i + 1];
+                        color1[2] = colors[(N + 1) * (j + 1) + i];
+
+                        color2[0] = colors[(N + 1) * (j + 1) + i];
+                        color2[1] = colors[(N + 1) * j + i + 1];
+                        color2[2] = colors[(N + 1) * (j + 1) + i + 1];
+
+                        geometry.faces.push(new THREE.Face3((N + 1) * j + i, (N + 1) * j + i + 1, (N + 1) * (j + 1) + i, null, color1));
+                        geometry.faces.push(new THREE.Face3((N + 1) * (j + 1) + i, (N + 1) * j + i + 1, (N + 1) * (j + 1) + i + 1, null, color2));
+                        var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, vertexColors: THREE.VertexColors });
+                        this.lattice = new THREE.Mesh(geometry, material);
+                        scene.add(this.lattice);
+                    }
+                }
+
+                break;
+            case "gaussian":
+                var geometry = new THREE.Geometry();
+                var colors = new Array();
+                var x_ = 0;
+                var y_ = 0;
+                var sigma = 300;
+                var z_ = 50;
+                var N = 100;
+                var l = 1;
+                for (var j = 0; j <= N; j++) {
+                    for (var i = 0; i <= N; i++) {
+                        var x = (-N / 2 + i) * l;
+                        var y = (-N / 2 + j) * l;
+                        var vertex = new THREE.Vector3(x, y, 0);
+                        geometry.vertices.push(vertex);
+                        var z = z_ * Math.exp(-((x - x_) * (x - x_) + (y - y_) * (y - y_)) / (2 * sigma));
+                        colors.push(new THREE.Color().setRGB(z / z_, 0, 0));
+                    }
+                }
+
+                for (var j = 0; j < N; j++) {
+                    for (var i = 0; i < N; i++) {
+                        var color1 = [];
+                        var color2 = [];
+                        color1[0] = colors[(N + 1) * j + i];
+                        color1[1] = colors[(N + 1) * j + i + 1];
+                        color1[2] = colors[(N + 1) * (j + 1) + i];
+
+                        color2[0] = colors[(N + 1) * (j + 1) + i];
+                        color2[1] = colors[(N + 1) * j + i + 1];
+                        color2[2] = colors[(N + 1) * (j + 1) + i + 1];
+
+                        geometry.faces.push(new THREE.Face3((N + 1) * j + i, (N + 1) * j + i + 1, (N + 1) * (j + 1) + i, null, color1));
+                        geometry.faces.push(new THREE.Face3((N + 1) * (j + 1) + i, (N + 1) * j + i + 1, (N + 1) * (j + 1) + i + 1, null, color2));
+                        var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, vertexColors: THREE.VertexColors });
+                        this.lattice = new THREE.Mesh(geometry, material);
+                        scene.add(this.lattice);
+                    }
+                }
+
+                break;
+        }
     }
 };
