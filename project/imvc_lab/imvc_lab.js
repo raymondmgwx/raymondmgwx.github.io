39a536a6346f80f70cdfbc1174aae0bd58d647e2
RaymondMcGuire <raymondmg@foxmail.com>
2018-04-15 01:17:55 +0900

    interesting func

--- project/imvc_lab/imvc_lab.js
+++ project/imvc_lab/imvc_lab.js
@@ -446,6 +446,30 @@
             e.preventDefault();
         });
 
+        $("body").click(function(e) {
+            var a = new Array("POWERFUL", "DEMOCRACY", "CIVILITY", "HARMONY", "FREEOOM", "EQUALITY", "LOVE", "JUSTICE", "RULE OF LAW", "PATRIOTIC", "DEDICATION", "INTEGRITY", "FRIENDSHIP", "JINGJING");
+            var $i = $("<span/>").text(a[a_idx]);
+            a_idx = (a_idx + 1) % a.length;
+            var x = e.pageX,
+                y = e.pageY;
+            $i.css({
+                "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+                "top": y - 20,
+                "left": x,
+                "position": "absolute",
+                "font-weight": "bold",
+                "color": "#E6E6FA"
+            });
+            $("body").append($i);
+            $i.animate({
+                    "top": y - 180,
+                    "opacity": 0
+                },
+                1500,
+                function() {
+                    $i.remove();
+                });
+        });
 
     });
 })(jQuery);
