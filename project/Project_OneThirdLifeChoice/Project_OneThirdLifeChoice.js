2f3b7d0b3243c29f41ea5cbb924fd4c4b6a26c3c
RaymondMcGuire <raymondmg@foxmail.com>
2018-05-25 00:32:23 +0900

    project and work describ

---
+++ project/Project_OneThirdLifeChoice/Project_OneThirdLifeChoice.js
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////                                                                      //
+////  Image-Visual-Lab      //////////   v0.0                                                              //
+//////////////////////////////////////                                                                      //
+//////////////////////////////////////  Copyright 2017-2018,                                                //
+//////////////////////////////////////  Last vist: 05, 24, 2018  by Raymond Wang                             //
+//////////////////////////////////////                                                                      //
+/////////////////////////////////////////////////////////////////////////////////////////////////WANG  XU///-->
+
+
+
+function addScript() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "./project/Project_OneThirdLifeChoice/Project-theme.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+
+addScript();
