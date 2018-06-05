064aa66ca686fabffaf24ae338334c216c38577f
RaymondMcGuire <raymondmg@foxmail.com>
2018-06-05 11:23:40 +0900

    combine three.js system
    TODO

--- project/ECS/ts_scripts/System.ts
+++ project/ECS/ts_scripts/System.ts
@@ -8,6 +8,7 @@
 /// <reference path="./LoadData.ts" />
 /// <reference path="./HashSet.ts" />
 module ECS {
+
     export class System {
         name: string;
         constructor(name: string) {
@@ -27,9 +28,9 @@ module ECS {
         }
         Execute() {
             super.Execute();
-            Utils.loadData('./data/tip.json',<JsonDataComponent>this.entities.get("tip_entity").components.get("jsondata"),function(){
+            Utils.loadData('./data/tip.json', <JsonDataComponent>this.entities.get("tip_entity").components.get("jsondata"), function () {
                 console.log("load tip data finished!");
-                Utils.loadData('./data/country.json',<JsonDataComponent>this.entities.get("country_entity").components.get("jsondata"),function(){
+                Utils.loadData('./data/country.json', <JsonDataComponent>this.entities.get("country_entity").components.get("jsondata"), function () {
                     console.log("load country data finished!");
                     //print json
                     console.log((<JsonDataComponent>this.entities.get("tip_entity").components.get("jsondata")).data);
